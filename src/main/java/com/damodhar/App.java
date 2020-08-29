6b90b644bf452f2a8c405ab91f373347623975cb
Damodhar Reddy <damodhar963@gmail.com>
2020-08-29 15:13:23 +0530

    first commit

---
+++ src/main/java/com/damodhar/App.java
@@ -0,0 +1,13 @@
+package com.damodhar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
